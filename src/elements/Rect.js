d779699b697703f1eca92d9f677a44cff80990bd
Andreas Gohr <gohr@cosmocode.de>
2025-04-29 18:47:46 +0200

    fix rect bounding box with stroke calculation

--- src/elements/Rect.js
+++ src/elements/Rect.js
@@ -11,13 +11,21 @@ export class Rect extends BaseElement {
         });
     }
 
+    /**
+     * This returns the visual bounding box of the rectangle including the stroke width.
+     *
+     * This is both correct (when we want to enclose the element) and incorrect (when we want to
+     * calculate or position it).
+     *
+     * @returns {{x: number, y: number, width: *, height: *}}
+     */
     getBoundingBox() {
-        // strokes are centered on the path, so we need to adjust the bounding box
+        // strokes are centered on the path, half outside and half inside
         const strokeWidth = this.attrs['stroke-width'] || 0;
-        const x = (parseFloat(this.attrs.x) || 0) - strokeWidth;
-        const y = (parseFloat(this.attrs.y) || 0) - strokeWidth;
-        const width = (parseFloat(this.attrs.width) || 0) + strokeWidth * 2;
-        const height = (parseFloat(this.attrs.height) || 0) + strokeWidth * 2;
+        const x = (parseFloat(this.attrs.x) || 0) - (strokeWidth/2);
+        const y = (parseFloat(this.attrs.y) || 0) - (strokeWidth/2);
+        const width = (parseFloat(this.attrs.width) || 0) + strokeWidth;
+        const height = (parseFloat(this.attrs.height) || 0) + strokeWidth;
 
         return {
             x,
